dfa2980c8c5ca9db58636e87cbe6972d5c2b8a48
Berezhnoy <berezhnoyaa@gmail.com>
2013-12-24 18:18:51 +0400

    -

---
+++ freeOld.php
@@ -0,0 +1,25 @@
+<?php
+$dir = "/tmp/obsdb"; // Директория с файлами
+$files = scandir( $dir ); // Прочёсываем директорию
+$time = time(); // Текущее время
+$life_file = 3600; // Время жизни файла в секундах
+ 
+// Отнимаем от текущего времени время жизни файла
+$time = $time - $life_file;
+ 
+// В цикле обходим массив
+foreach( $files as $file )
+{
+    if( $file != "." && $file != ".." )
+    {
+        $file = $dir.$file;
+        $filemtime = filemtime( $file ); // Время создания или модификации файла
+                
+        // Удаляем, если нужно
+        if( $filemtime <= $time )
+        {
+            unlink( $file );
+        }
+    }
+}
+ 
